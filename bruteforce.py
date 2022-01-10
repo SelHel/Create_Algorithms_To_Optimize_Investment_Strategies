0624eadcae85618ceb51d2d327f0b483525c0601
Helaoui Selim <helsel@MacBook-Pro-de-Helaoui.local>
2022-01-10 17:29:37 +0100

    Correction PEP 8.

--- bruteforce.py
+++ bruteforce.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from itertools import combinations
 from pprint import pprint as pp
@@ -27,17 +28,17 @@ def bruteforce(action_list):
     possibilities = []
     for c in comb:
         dict_tmp = {}
-        price = 0
+        total_cost = 0
         gain = 0
         for action in action_list:
             if action not in c:
                 dict_tmp[action[0]] = "NO BUY"
             else:
                 dict_tmp[action[0]] = "BUY"
-                price += float(action[1]) / 100
-                gain += float(action[2]) / 100
-        dict_tmp["price"] = price
-        dict_tmp["gain"] = gain
+                total_cost += float(action[1]) / 100
+                gain += float(action[3]) / 100
+        dict_tmp["Investissement"] = total_cost
+        dict_tmp["Gain"] = gain
         possibilities.append(dict_tmp)
     write_on_csv(possibilities, BUDGET)
     return possibilities
@@ -54,20 +55,26 @@ def best_investment(possibilities):
 
     Retour
     ------
-    Retoune la liste des meilleures actions à acheter, le gain et le coût total.
+    Retoune la liste des meilleures actions à acheter, le gain et total.
     """
     best_decision = {}
     max_profit = 0
     for p in possibilities:
-        if p["price"] <= BUDGET and p["gain"] > max_profit:
-            max_profit = p["gain"]
+        if p["Investissement"] <= BUDGET and p["Gain"] > max_profit:
+            max_profit = p["Gain"]
             best_decision = p
     return best_decision
 
 
 if __name__ == "__main__":
+    file_args = sys.argv
+    if len(file_args) >= 2:
+        filename = file_args[1]
+    else:
+        filename = "data/actions.csv"
+    print("------------------------ Algorithme de force brute ------------------------")
     start = time.time()
-    actions = read_and_clean_csv("csv/actions.csv")
+    actions = read_and_clean_csv(filename)
     possibilities = bruteforce(actions)
     pp(best_investment(possibilities))
     print(f"Temps d’exécution de l'algorithme : {time.time() - start} secondes.")
